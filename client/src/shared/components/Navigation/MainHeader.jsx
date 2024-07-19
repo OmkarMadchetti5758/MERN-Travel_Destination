886532c257964ad3c44afbc08fd66ee09fa93726
OmkarMadchetti5758 <151734201+OmkarMadchetti5758@users.noreply.github.com>
2024-07-19 20:53:13 +0530

    Delete client directory

--- client/src/shared/components/Navigation/MainHeader.jsx
+++
@@ -1,9 +0,0 @@
-// import './MainHeader.css'
-
-export default function MainHeader(props) {
-  return (
-    <header className="w-full h-16 bg-cyan-950 flex items-center fixed top-0 left-0 shadow-slate-950 px-3 py-10 z-100 md:justify-between">
-      {props.children}
-    </header>
-  );
-}
